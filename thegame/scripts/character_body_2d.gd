cd07eb62f949ae50222547aaca9e77a2352b3b8b
ITSCrew-Keith <seaglethebeagle@gmail.com>
2024-12-11 20:45:54 -0600

    version 0.42
    version 0.42

--- thegame/scripts/character_body_2d.gd
+++ thegame/scripts/character_body_2d.gd
@@ -3,26 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
+var airjump = false
 
 
-@onready var animated_sprite = %AnimatedSprite2D
+@onready var animated_sprite = %PLAYER_SPRITE
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		airjump = true
 		velocity += get_gravity() * delta
+		animated_sprite.play("jump_start")
 		
 		
-
-
-	# Handle jump.
+	if is_on_floor():
+		airjump = false
 	if (Input.is_key_pressed(KEY_SPACE) and is_on_floor()):
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY - (Global.coin * 17) - (Global.superjump * 500)
+		velocity.y = JUMP_VELOCITY - (Global.coin * 13) - (Global.superjump * 500)
 		if Global.superjump >= 1:
 			Global.superjump = 0
-
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := 0
@@ -41,10 +41,11 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
-	if direction == 0:
+	if direction == 0 and airjump == false:
 		animated_sprite.play("idle")
-	else:
+	elif airjump == false:
 		animated_sprite.play("run")
+	
 	move_and_slide()
 	
 func boucepad_jump():
